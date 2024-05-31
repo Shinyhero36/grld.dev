c8c904ef1ea9470c24ccdc3b025eab7072a1111c
Gérald Leban <gerald.leban0@gmail.com>
2024-05-31 19:26:25 +0200

    feat: add projects

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	interface Props {
+		title: string;
+		description: string;
+		slug: string;
+
+		children?: any;
+	}
+
+	const { title, description, slug, children }: Props = $props();
+</script>
+
+<a href="/projects/{slug}">
+	<div class="project">
+		<div class="preview">
+			{#if children}
+				{@render children()}
+			{/if}
+		</div>
+		<div class="info">
+			<h3 class="text-xl">{title}</h3>
+			<p class="text-md">{description}</p>
+		</div>
+	</div>
+</a>
+
+<style lang="scss">
+	a {
+		text-decoration: none;
+		color: inherit;
+
+		:visited {
+			color: inherit;
+		}
+	}
+
+	.project {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+
+		background-color: var(--bg-component-subtle-neutral);
+		border-radius: var(--rounded-3xl);
+
+		height: 100%;
+
+		.info {
+			padding: var(--spacing-3xl);
+		}
+
+		.info > p {
+			color: var(--text-subtle-neutral);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,51 @@
 <script>
+	import Footer from '$lib/components/Footer.svelte';
 	import Hero from '$lib/components/Hero.svelte';
+	import ProjectCard from '$lib/components/ProjectCard.svelte';
+
+	const projects = [
+		{
+			title: 'What The Hex ?!',
+			description: 'A wordle-like game but instead of words, it’s hex colors.',
+			slug: 'what-the-hex'
+		},
+		{
+			title: 'Svote',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+			slug: 'svote'
+		}
+	];
 </script>
 
 <Hero />
+
+<div class="projects">
+	{#each projects as project}
+		<ProjectCard {...project}>
+			<div style="height: 150px;"></div>
+		</ProjectCard>
+	{/each}
+</div>
+
+<Footer />
+
+<style lang="scss">
+	.projects {
+		display: grid;
+		grid-auto-rows: minmax(0, 1fr);
+		grid-template-columns: repeat(5, minmax(auto, 1fr));
+
+		max-width: 1200px;
+		margin: var(--spacing-4xl) auto;
+
+		gap: var(--spacing-3xl);
+
+		:global(> :nth-child(1)) {
+			grid-column: auto / span 2;
+		}
+		:global(> :nth-child(2)) {
+			grid-column: auto / span 3;
+		}
+	}
+</style>
